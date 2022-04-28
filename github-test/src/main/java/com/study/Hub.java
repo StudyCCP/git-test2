93b7f34d5f6c41d69c315cea8095472b7023af7a
Study <ccp@study.com>
2022-04-28 15:06:36 +0800

    2222sss

---
+++ github-test/src/main/java/com/study/Hub.java
@@ -0,0 +1,8 @@
+package com.study;
+
+public class Hub {
+	private void mian() {
+		// TODO Auto-generated method stub
+	System.out.println("hello");
+	}
+}
---
+++ github-test/src/main/resources/com/study/heh/Goods.java
@@ -0,0 +1,7 @@
+package com.study.heh;
+
+public class Goods {
+	public static void main(String[] args) {
+		System.out.println("บวบวบว");
+	}
+}
